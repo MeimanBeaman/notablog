27d47c00f2d1603829fddabcc3e552e130ddb6e1
Meiman Bimendin <meyman98@gmail.com>
2020-09-09 20:27:57 +0600

    add jpa

---
+++ src/main/java/org/project/notablog/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package org.project.notablog.repos;
+
+import org.project.notablog.domains.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}

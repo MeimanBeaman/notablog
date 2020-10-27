84a0252019060d5c0033611d828c117f53fe9e44
Meiman Bimendin <meyman98@gmail.com>
2020-10-27 19:39:46 +0600

    Add Email

--- src/main/java/org/project/notablog/service/UserService.java
+++ src/main/java/org/project/notablog/service/UserService.java
@@ -1,19 +1,77 @@
 package org.project.notablog.service;
 
+import org.project.notablog.domains.Role;
+import org.project.notablog.domains.User;
 import org.project.notablog.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private MailService mailService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        User userMailCheck = userRepo.findByEmail(user.getEmail());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        if (userMailCheck != null) {
+            return false;
+        }
+
+        user.setActive(false);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+
+        //TODO Вынести адрес сайта в property файл
+        if(!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Not a Blog. To activate your account follow the link: http://localhost:8080/activate/%s",
+                            user.getUsername(),
+                            user.getActivationCode()
+            );
+
+            mailService.send(user.getEmail(), "Activation code for your account in Not a Blog", message);
+        }
+
+        return true;
+
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+
+        if (user==null) {
+            return false;
+        }
+
+        user.setActivationCode(null);
+        user.setActive(true);
+
+        userRepo.save(user);
+
+        return true;
     }
 }
